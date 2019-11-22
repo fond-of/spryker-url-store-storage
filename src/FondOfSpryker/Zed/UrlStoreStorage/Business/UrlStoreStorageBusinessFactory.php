a66052d1362e99d8a7e2b3159b9d3427202e69ec
Daniel Kemna <danielkemna@t-online.de>
2019-11-22 12:18:08 +0100

    multi store rewrite

--- src/FondOfSpryker/Zed/UrlStoreStorage/Business/UrlStoreStorageBusinessFactory.php
+++ src/FondOfSpryker/Zed/UrlStoreStorage/Business/UrlStoreStorageBusinessFactory.php
@@ -2,8 +2,11 @@
 
 namespace FondOfSpryker\Zed\UrlStoreStorage\Business;
 
+use FondOfSpryker\Zed\CategoryExtendStorage\CategoryExtendStorageDependencyProvider;
+use FondOfSpryker\Zed\UrlStoreStorage\UrlStoreStorageDependencyProvider;
 use Spryker\Shared\Log\LoggerTrait;
 use FondOfSpryker\Zed\UrlStoreStorage\Business\Storage\UrlStorageWriter;
+use Spryker\Zed\Store\Business\StoreFacadeInterface;
 use Spryker\Zed\UrlStorage\Business\UrlStorageBusinessFactory as SprykerUrlStorageBusinessFactory;
 
 /**
@@ -13,14 +16,25 @@ use Spryker\Zed\UrlStorage\Business\UrlStorageBusinessFactory as SprykerUrlStora
 class UrlStoreStorageBusinessFactory extends SprykerUrlStorageBusinessFactory
 {
     /**
-     * @return \Spryker\Zed\UrlStorage\Business\Storage\UrlStorageWriterInterface
+     * @return \FondOfSpryker\Zed\UrlStoreStorage\Business\Storage\UrlStorageWriter|\Spryker\Zed\UrlStorage\Business\Storage\UrlStorageWriterInterface
      */
-    public function createUrlStorageWriter()
+    public function createUrlStorageWriter(): UrlStorageWriter
     {
         return new UrlStorageWriter(
             $this->getUtilSanitizeService(),
             $this->getQueryContainer(),
-            $this->getConfig()->isSendingToQueue()
+            $this->getConfig()->isSendingToQueue(),
+            $this->getStoreFacade()
         );
+    }
+
+    /**
+     * @throws
+     *
+     * @return \Spryker\Zed\Store\Business\StoreFacadeInterface
+     */
+    protected function getStoreFacade(): StoreFacadeInterface
+    {
+        return $this->getProvidedDependency(UrlStoreStorageDependencyProvider::FACADE_STORE);
     }
 }
