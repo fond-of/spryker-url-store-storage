f99a7f6efd7f041f9fdb1295317538e81c1757ab
SpiderDan98 <40319023+SpiderDan98@users.noreply.github.com>
2019-12-19 11:38:20 +0100

    multi store rewrite (#1)
    * multi store rewrite
    
    * improve code style

--- src/FondOfSpryker/Zed/UrlStoreStorage/UrlStoreStorageDependencyProvider.php
+++ src/FondOfSpryker/Zed/UrlStoreStorage/UrlStoreStorageDependencyProvider.php
@@ -2,8 +2,37 @@
 
 namespace FondOfSpryker\Zed\UrlStoreStorage;
 
+use Spryker\Zed\Kernel\Container;
 use Spryker\Zed\UrlStorage\UrlStorageDependencyProvider as SprykerUrlStorageDependencyProvider;
 
 class UrlStoreStorageDependencyProvider extends SprykerUrlStorageDependencyProvider
 {
+    public const FACADE_STORE = 'FACADE_STORE';
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    public function provideBusinessLayerDependencies(Container $container): Container
+    {
+        $container = parent::provideBusinessLayerDependencies($container);
+        $container = $this->addStoreFacade($container);
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function addStoreFacade(Container $container): Container
+    {
+        $container[static::FACADE_STORE] = function (Container $container) {
+            return $container->getLocator()->store()->facade();
+        };
+
+        return $container;
+    }
 }
